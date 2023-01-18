1ca42713fdeef7afe743855dcede8e9add470c47
Yash Patidar <yashpatidar964@gmail.com>
2023-01-18 13:04:40 +0530

    initial commit practice question 12

---
+++ practice-question-12/app12.js
@@ -0,0 +1,22 @@
+var statusButton = document.querySelector(".status");
+var output = document.querySelector(".outputArea");
+var tasks = [
+    { name: "cleaning", completed: true },
+    { name: "washing", completed: false },
+    { name: "driving", completed: true }
+  ];
+statusButton.addEventListener("click", checkStatus(tasks));
+
+
+
+//console.log(tasks);
+function checkStatus(tasks) {
+    for(var i = 0; i < tasks.length; i++) {
+        if(tasks[i].completed === true) {
+            console.log(tasks[i].name);
+            output.innerText = tasks[i].name
+        }else{
+            output.innerText = tasks[i].name + " is not finisehd";
+        }
+    }
+}
---
+++ practice-question-12/readme.md
@@ -0,0 +1,1 @@
+Create a list of 3 objects that has 2 attributes name of the task and completed which is a boolean value true or false. Display those tasks which are completed or which have attributes as completed:true on the click of a button.
